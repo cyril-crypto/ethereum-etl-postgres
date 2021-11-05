11e334b6e6dc35e84ce109158031fc16fd1ce45a
cyrilwong <cyril.wong@crypto.com>
2021-11-05 13:49:27 +0800

    update index for cronos

--- indexes/token_transfers.sql
+++ indexes/token_transfers.sql
@@ -5,3 +5,5 @@ create index token_transfers_block_timestamp_index on token_transfers (block_tim
 create index token_transfers_token_address_block_timestamp_index on token_transfers (token_address, block_timestamp desc);
 create index token_transfers_from_address_block_timestamp_index on token_transfers (from_address, block_timestamp desc);
 create index token_transfers_to_address_block_timestamp_index on token_transfers (to_address, block_timestamp desc);
+
+CREATE INDEX token_transfers_block_number_token_address ON logs (block_number, token_address);
